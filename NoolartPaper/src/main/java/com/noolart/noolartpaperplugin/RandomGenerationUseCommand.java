6d131ff0dacfac28f10ea38842bf26822f459635
Utochkin Stas <utochkins@bk.ru>
2022-03-01 16:46:48 +0700

    вынес код вставки в отдельный класс и произвел соответствующий рефакторинг по изменению кода

--- NoolartPaper/src/main/java/com/noolart/noolartpaperplugin/RandomGenerationUseCommand.java
+++ NoolartPaper/src/main/java/com/noolart/noolartpaperplugin/RandomGenerationUseCommand.java
@@ -1,18 +1,13 @@
 package com.noolart.noolartpaperplugin;
 
-import org.bukkit.Bukkit;
+import com.noolart.noolartpaperplugin.csv.CsvInserter;
 import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class RandomGenerationUseCommand implements CommandExecutor {
@@ -51,9 +46,6 @@ public class RandomGenerationUseCommand implements CommandExecutor {
 
         HashMap<Integer, File> string_models_with_probabilityes = new HashMap<>();
         Location point_to_generate = NoolartPaperPlugin.point1;
-
-        int model_counter = 1;
-        int allmodels = (int) Math.pow(generation_length, 2);
 
         int universal_counter = 0;
 
@@ -108,10 +100,9 @@ public class RandomGenerationUseCommand implements CommandExecutor {
 
         for (Map.Entry<Integer, File> item : string_models_with_probabilityes.entrySet()) {
             keys[universal_counter] = item.getKey();
-//            Bukkit.broadcastMessage(item.getValue().getPath());
-//            Bukkit.broadcastMessage(item.getKey().toString());
             universal_counter++;
         }
+
         Arrays.sort(keys);
 
         File path = null;
@@ -136,79 +127,8 @@ public class RandomGenerationUseCommand implements CommandExecutor {
                     }
                 }
 
-                Scanner scan = null;
+                CsvInserter.paste(path);
 
-                try {
-                    scan = new Scanner(path);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-
-                String name = sender.getName();
-                Player p = Bukkit.getPlayer(name);
-
-                int cx1 = (int) point_to_generate.getX();
-                int cy1 = (int) point_to_generate.getY();
-                int cz1 = (int) point_to_generate.getZ();
-
-                String string = scan.nextLine();
-
-                String[] first_line = string.split(";");
-
-                int m1 = Integer.parseInt(first_line[0]);
-                int m2 = Integer.parseInt(first_line[1]);
-                int m3 = Integer.parseInt(first_line[2]);
-
-                while (scan.hasNextLine()) {
-                    StringBuilder text = new StringBuilder();
-
-                    Bukkit.broadcastMessage("Model " + model_counter + " / " + allmodels);
-                    model_counter++;
-
-                    int c1 = 0;
-                    int c2 = 1;
-                    int c3 = 2;
-                    int k = 3;
-
-
-                    for (int r1 = 0; r1 <= 10000; r1++) {
-                        if (scan.hasNextLine()) {
-                            text.append(scan.nextLine());
-                        } else {
-                            break;
-                        }
-                    }
-
-                    String[] symbols = text.toString().split(";");
-                    int scanlength = symbols.length / 4;
-
-                    while (scanlength > 0) {
-                        String materialName = symbols[k].toUpperCase();
-                        Material theMaterial = null;
-                        try {
-                            theMaterial = Material.valueOf(materialName);
-                        } catch (Exception e1) {
-                            //Not a valid material
-                        }
-                        World w = p.getWorld();
-
-                        double dx = cx1 + Integer.parseInt(symbols[c1]) - m1;
-                        double dy = cy1 + Integer.parseInt(symbols[c2]) - m2;
-                        double dz = cz1 + Integer.parseInt(symbols[c3]) - m3;
-
-                        Location h1 = new Location(w, dx, dy, dz);
-//                Bukkit.broadcastMessage(h1.toString());
-                        h1.getBlock().setType(theMaterial);
-                        c1 = c1 + 4;
-                        c2 = c2 + 4;
-                        c3 = c3 + 4;
-                        k = k + 4;
-                        scanlength--;
-
-                    }
-                }
-                scan.close();
-//                Bukkit.broadcastMessage("Done!");
                 point_to_generate.setZ(point_to_generate.getZ() + model_length);
 
             }
@@ -219,4 +139,3 @@ public class RandomGenerationUseCommand implements CommandExecutor {
         return true;
     }
 }
-
